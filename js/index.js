e8efba277fce3f688723bb3794b5e864dc3ee800
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-09 00:09:17 +0300

    add changes to javascript file

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('h1').click(function(){
+        alert('this is a header');
+    })
+    $('p').click(function(){
+        alert('this is a paragrapgh');
+    })
+    $('img').click(function(){
+        alert('this is an image');
+    }
+    )
+});
+// jQuery('#h1').click(function(){
+//     alert("This is a heading tag")
+//   });
+
+//   jQuery('p').click(function(){
+//     alert("This is a paragraph")
+//   });
+
+//   jQuery('img').click(function(){
+//     alert("This is an image")
+//   });
